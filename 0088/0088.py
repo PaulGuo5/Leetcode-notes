8c0d286fffba397c4e765260d77b1fe20d911ce2
PaulGuo5 <zhg26@pitt.edu>
2019-11-13 10:12:33 -0500

    11.13

--- 0088/0088.py
+++ 0088/0088.py
@@ -1,16 +1,12 @@
-class Solution(object):
-    def merge2(self, nums1, m, nums2, n):
+class Solution:
+    def merge2(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
         nums1[m:] = nums2[:n]
-        return nums1.sort()
+        nums1.sort()
     
-    def merge(self, nums1, m, nums2, n):
+    def merge1(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         i = m - 1
         j = n - 1
         max_pst = m + n - 1
@@ -30,4 +26,34 @@ class Solution(object):
             nums1[max_pst] = nums2[j]
             j -= 1
             max_pst -= 1
-        return nums1
+            
+            
+    def merge3(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        nums1_copy = nums1[:m]
+        nums1[:] = []
+        
+        p1, p2 = 0, 0
+        while p1 < m and p2 < n:
+            if nums1_copy[p1] < nums2[p2]:
+                nums1.append(nums1_copy[p1])
+                p1 += 1
+            else:
+                nums1.append(nums2[p2])
+                p2 += 1
+        if p1 < m:
+            nums1[(p1 + p2):] = nums1_copy[p1:]
+        if p2 < n:
+            nums1[(p1 + p2):] = nums2[p2:]
+            
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        p1, p2 = m-1, n-1
+        p = m + n - 1 
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] < nums2[p2]:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            else:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            p -= 1
+        nums1[:p2+1] = nums2[:p2+1]
