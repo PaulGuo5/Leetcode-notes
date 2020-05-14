4e4597499827c8bcd264e13e553a58142a9293a6
PaulGuo5 <zhg26@pitt.edu>
2020-05-14 15:13:26 -0400

    2020-5-14

--- 1268/1268.py
+++ 1268/1268.py
@@ -1,14 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.words = []
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+            node.words.append(word)
+            node.words.sort()
+            while len(node.words) > 3:
+                node.words.pop()
+    
+    def search(self, word):
+        res = []
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                break
+            node = node.children[c]
+            res.append(node.words[:])
+        l_remain = len(word) - len(res)
+        for _ in range(l_remain):
+            res.append([])
+        return res
+            
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products.sort()
-        res, i, prefix = [], 0, ""
-        for w in searchWord:
-            prefix += w
-            i =  bisect.bisect_left(products, prefix, i)
-            tmp = []
-            for p in products[i:i+3]:
-                # if p.startswith(prefix):
-                if p[:len(prefix)] == prefix:
-                    tmp.append(p)
-            res.append(tmp)
-        return res
+        trie = Trie()
+        for prod in products:
+            trie.insert(prod)
+        return trie.search(searchWord)        
