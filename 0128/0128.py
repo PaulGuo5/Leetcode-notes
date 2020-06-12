a2a65a1712824db93648a29e112a4e938b2d90ad
PaulGuo5 <zhg26@pitt.edu>
2020-06-11 20:37:10 -0400

    2020-6-11

--- 0128/0128.py
+++ 0128/0128.py
@@ -1,16 +1,12 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        res, d = 0, {}
+        nums = set(nums)
+        res = 0
         for n in nums:
-            if n not in d:
-                left = d.get(n-1, 0)
-                right = d.get(n+1, 0)
-                
-                l = left+right+1
-                res = max(res, l)
-                
-                d[n] = l
-                d[n-left] = l
-                d[n+right] = l
-
+            if n-1 not in nums:
+                cnt = 1
+                while n+1 in nums:
+                    cnt += 1
+                    n += 1
+                res = max(res, cnt)
         return res
