292c711581020499fa647cf344708e2b680134e1
PaulGuo5 <zhg26@pitt.edu>
2020-07-08 12:27:55 -0400

    2020-7-8

--- 0146/0146.py
+++ 0146/0146.py
@@ -1,34 +1,75 @@
-class LRUCache:
+class LRUCache1:
+
     def __init__(self, capacity: int):
         self.cap = capacity
-        # self.cache = collections.OrderedDict()
-        self.cache = {}
-        self.order = []
-    def get(self, key: int) -> int:
-        # if key in self.cache.keys():
-        #     val = self.cache.pop(key)
-        #     self.cache[key] = val
-        #     return self.cache[key]
-        # return -1
-        if key in self.cache.keys():
-            self.order.remove(key)
-            self.order.append(key)
-            return self.cache[key]
-        return -1
-    def put(self, key: int, value: int) -> None:
-        # if key in self.cache:
-        #     self.cache.pop(key)
-        # elif len(self.cache) == self.cap:
-        #     self.cache.popitem(last = False)
-        # self.cache[key] = value
-        if key in self.cache:
-            self.order.remove(key)
-        elif len(self.order) == self.cap:
-                self.cache.pop(self.order.pop(0))
-        self.order.append(key)
-        self.cache[key] = value
-            
+        self.od = collections.OrderedDict()
 
+    def get(self, key: int) -> int:
+        if key in self.od:
+            self.od.move_to_end(key)
+            return self.od[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.od:
+            self.od.move_to_end(key)
+            self.od[key] = value
+        else:
+            if len(self.od) >= self.cap:
+                self.od.popitem(last = False)
+            self.od[key] = value
+            
+class Node:
+    def __init__(self, k, val):
+        self.k = k
+        self.val = val
+        self.prev = None
+        self.next = None
+        
+class LRUCache:
+    def __init__(self, capacity:int):
+        self.cap = capacity
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.table = {}
+        
+    def get(self, key):
+        if key not in self.table:
+            return -1
+        else:
+            node = self.table[key]
+            self._delete(node)
+            self._add(node)
+            return node.val
+        
+    def put(self, key, val):
+        if key in self.table:
+            node = self.table[key]
+            self._delete(node)
+        new = Node(key, val)
+        self._add(new)
+        self.table[key] = new
+        if len(self.table) > self.cap:
+            node = self.head.next
+            del self.table[node.k]
+            self._delete(node)
+            
+            
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        node.prev = p
+        self.tail.prev = node
+        node.next = self.tail
+        
+    def _delete(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+        
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
