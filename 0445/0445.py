e376525358ef82df09c59d77f87c035d1ebda38e
PaulGuo5 <zhg26@pitt.edu>
2020-07-27 14:35:33 -0400

    2020-7-27

--- 0445/0445.py
+++ 0445/0445.py
@@ -1,22 +1,47 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        c1, c2 = "", ""
-        while l1:
-            c1 += str(l1.val)
-            l1 = l1.next
-        while l2:
-            c2 += str(l2.val)
-            l2 =l2.next
-        sum_ = int(c1) + int(c2)
+        def getLen(l):
+            len_ = 0
+            while l:
+                len_ += 1
+                l = l.next
+            return len_
+        
+        def helper(l1, l2, diff):
+            if not l1:
+                return None
+            if diff == 0:
+                cur = ListNode(l1.val+l2.val)
+            else:
+                cur = ListNode(l1.val)
+            
+            if diff == 0:
+                carry = helper(l1.next, l2.next, diff)
+            else:
+                carry = helper(l1.next, l2, diff-1)
+            
+            if carry and carry.val//10 > 0:
+                cur.val += 1
+                carry.val %= 10
+            cur.next = carry
+            return cur
+        
+        len_1, len_2 = getLen(l1), getLen(l2)
+        if len_1 < len_2:
+            return self.addTwoNumbers(l2, l1)
+        diff = len_1 - len_2
         dummy = ListNode(0)
-        cur = dummy
-        for i in str(sum_):
-            cur.next = ListNode(int(i))
-            cur = cur.next
+        dummy.next = helper(l1, l2, diff)
+        
+        if dummy.next.val//10 > 0:
+            dummy.next.val %= 10
+            dummy.val += 1
+            return dummy
+        
         return dummy.next
+        
