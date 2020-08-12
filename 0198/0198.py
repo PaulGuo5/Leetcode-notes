aec094460ea57db1e0721feb31c1e6ea71cb84d9
PaulGuo5 <zhg26@pitt.edu>
2020-08-12 12:48:42 -0400

    2020-8-12

--- 0198/0198.py
+++ 0198/0198.py
@@ -1,21 +1,28 @@
-class Solution:
+            class Solution:
     def rob1(self, nums: List[int]) -> int:
-        #dp
         if not nums:
             return 0
-        if len(nums) == 1:
+        n = len(nums)
+        if n == 1:
             return nums[0]
-        dp = [0]*len(nums)
-        for i in range(len(nums)):
-            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+        
+        dp = [0]*(n+1)
+        dp[1] = nums[0]
+        for i in range(2, n+1):
+            dp[i] = max(nums[i-1]+dp[i-2], dp[i-1])
+            
         return dp[-1]
     
     def rob(self, nums: List[int]) -> int:
-        pre, cur = 0, 0
-        for n in nums:
-            tmp = pre #i-2
-            pre = cur #i-1
-            cur = max(n + tmp, pre)
-            # cur, pre = max(n+pre, cur), cur
-        return cur
-            
+        if not nums:
+            return 0
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        
+        old, now = 0, nums[0]
+        for i in range(2, n+1):
+            tmp = max(nums[i-1]+old, now)
+            old = now
+            now = tmp
+        return now
