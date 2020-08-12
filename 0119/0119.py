a3cb2aa3f6ae00c565b29ad526a4a2ae16732368
PaulGuo5 <zhg26@pitt.edu>
2020-08-12 10:58:53 -0400

    2020-8-12

--- 0119/0119.py
+++ 0119/0119.py
@@ -1,18 +1,22 @@
 class Solution:
-    def getRow(self, rowIndex: int) -> List[int]:
-        rowNum = rowIndex + 1
-        if rowNum == 0:
-            return []
-        if rowNum ==1:
+    def getRow1(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
             return [1]
-        res=[[1]]
-        for i in range(1, rowNum):
-            row = []
-            row.append(1)
-            if i > 1: 
-                for j in range(1, i):
-                    row.append(lastRow[j-1] + lastRow[j])
-            row.append(1)
-            lastRow = row
-        
+        if rowIndex == 1:
+            return [1,1]
+        rows = [[] for _ in range(rowIndex+1)]
+        rows[0].append([1])
+        rows[1] += [1,1]
+        for i in range(2, rowIndex+1):
+            len_ = i+1
+            rows[i].append(1)
+            for j in range(1, len_-1):
+                rows[i] += [rows[i-1][j-1]+rows[i-1][j]]
+            rows[i].append(1)
+        return rows[-1]
+    
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for _ in range(rowIndex):
+            row = [x + y for x, y in zip([0]+row, row+[0])]
         return row
