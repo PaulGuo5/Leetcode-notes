60972933dfa78752c7133674dda95462171c54e6
PaulGuo5 <zhg26@pitt.edu>
2020-10-15 16:25:21 -0400

    2020-10-15

--- notes/0189/0189.py
+++ notes/0189/0189.py
@@ -1,29 +1,19 @@
 class Solution:
-    def reverseList(self, nums, start, end):
-        if start >= end:
-            return nums
-        ps = start
-        pe = end
-        while start < end:
-            temp = nums[start]
-            nums[start] = nums[end]
-            nums[end] = temp
-            start += 1
-            end -= 1
-        return nums
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        length = len(nums)
-        if length > 1:
-            if length <= k:
-                i = 0
-                while i < k:
-                    self.reverseList(nums, 0, length-1)
-                    self.reverseList(nums, 1, length-1)
-                    i += 1
-            else:
-                self.reverseList(nums, 0, length-1)
-                self.reverseList(nums, 0, k-1)
-                self.reverseList(nums, k, length-1)
+        if not nums:
+            return None
+        n = len(nums)
+        k %= n
+        
+        def reverse(s, e):
+            while s < e:
+                nums[s], nums[e] = nums[e], nums[s]
+                s += 1
+                e -= 1
+        
+        reverse(0, n-k-1)
+        reverse(n-k, n-1)
+        reverse(0, n-1)
